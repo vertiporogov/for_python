1270e733cd3eb8fc2165ddd3c7737a413087e148
vertiporogov <vertiporogov.alexandr@mail.ru>
2023-06-03 14:23:20 +0300

    first commit

---
+++ funcs.py
@@ -0,0 +1,7 @@
+
+def sum__(one, two):
+    sum_ = one + two
+
+    return sum_
+
+# print(sum__())
---
+++ main.py
@@ -0,0 +1,7 @@
+
+from funcs import sum__
+
+if __name__ == '__main__':
+    # print('jj')
+    print(sum__(25, 50))
+
