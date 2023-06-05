8a435a8b759c28933883a2c07e6eb0d2b458bfd7
vertiporogov <vertiporogov.alexandr@mail.ru>
2023-06-05 19:50:03 +0300

    used the sudtraction function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-from funcs import sum__
+from funcs import sum__, sudtraction
 
 if __name__ == '__main__':
 
-    print(sum__(25, 155))
+    # print(sum__(25, 155))
+    print(sudtraction(25, 155))
 
